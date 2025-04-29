5fe8fd46af70f7e12858e39b47c724e0d9dd8dfb
evroman03 <evroman@mail.bradley.edu>
2025-04-29 17:11:28 -0500

    LFUh
    Made yesterday. Inherits from CacheBase and is set up for the cpp

---
+++ FinalProject/Milestone1/LFU.cpp
@@ -0,0 +1,61 @@
+#include "LFU.h"
+#include "iostream"
+
+template<typename Key, typename Value>
+void LFUCache<Key, Value>::put(const Key& key, const Value& value) 
+{
+    if (CacheBase<Key, Value>::CAPACITY == 0)
+    {
+        cout << "Non-zero capacity required to input something"
+        return;
+    }
+
+    //update value and return early if found
+    if (get(key)) 
+    {
+        cache[key].value = value;
+        return;
+    }
+
+    //otherwise, THEN the cache must be full
+    if (cache.size() >= CacheBase<Key, Value>::CAPACITY) 
+    {
+        //use minFreq to get the frontmost, FIRSTmost in the freqMap
+        Key evictKey = freqMap[minFreq].front();
+        freqMap[minFreq].pop_front();
+        cache.erase(evictKey);
+    }
+
+    freqMap[1].push_back(key);
+    cache[key] = { value, 1, --freqMap[1].end() };
+    minFreq = 1;
+}
+
+template<typename Key, typename Value>
+Value* LFUCache<Key, Value>::get(const Key& key) 
+{
+    auto it = cache.find(key);
+
+    //cache.end is the "cliff" marker that indicates a not found
+    if (it == cache.end()) 
+    {
+        return nullptr;
+    }
+
+    int freq = it->second.frequency;
+    freqMap[freq].erase(it->second.listIter);
+
+    if (freqMap[freq].empty() && freq == minFreq) 
+    {
+        ++minFreq;
+    }
+
+    freqMap[freq + 1].push_back(key);
+    it->second.frequency++;
+    it->second.listIter = --freqMap[freq + 1].end();
+
+    return &it->second.value;
+}
+
+// Force template instantiation if needed elsewhere
+//template class LFUCache<std::string, int>; // Example types
