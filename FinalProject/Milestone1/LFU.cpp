35dfe5ad8bc27af433935318f6512c4d75826580
evroman03 <evroman@mail.bradley.edu>
2025-04-29 17:22:49 -0500

    Added more comments to LFUcpp and correctly pushed LFUh

--- FinalProject/Milestone1/LFU.cpp
+++ FinalProject/Milestone1/LFU.cpp
@@ -27,6 +27,7 @@ void LFUCache<Key, Value>::put(const Key& key, const Value& value)
     }
 
     freqMap[1].push_back(key);
+    //make a value with the freq set to 1 and its pointer to the first bucket's end, but one to the left/positive/-1
     cache[key] = { value, 1, --freqMap[1].end() };
     minFreq = 1;
 }
@@ -42,16 +43,22 @@ Value* LFUCache<Key, Value>::get(const Key& key)
         return nullptr;
     }
 
+    //if we did find a value in the data map, store its frequency
     int freq = it->second.frequency;
+    //remove this value from the freqMap because it is changing frequency
     freqMap[freq].erase(it->second.listIter);
 
+    //
     if (freqMap[freq].empty() && freq == minFreq) 
     {
         ++minFreq;
     }
 
+    //re-add the value, but in a high frequency bucket
     freqMap[freq + 1].push_back(key);
+    //increment the value's frequency
     it->second.frequency++;
+    //set the listiter to the next frequency bucket, (freq+1), one before the end sentinal
     it->second.listIter = --freqMap[freq + 1].end();
 
     return &it->second.value;
---
+++ FinalProject/Milestone1/LFU.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CacheBase.h"
+#include <unordered_map>
+#include <list>
+
+template<typename Key, typename Value>
+class LFUCache : public CacheBase<Key, Value> 
+{
+
+public:
+    void put(const Key& key, const Value& value) override;
+    Value* get(const Key& key) override;
+
+private:
+    struct CacheEntry 
+    {
+        Value value;
+        int frequency;
+
+        //iterator to the freqMap
+        typename std::list<Key>::iterator listIter;
+    };
+
+    //our hashmap using the cache entry. Every time an entry is used, its freq goes up
+    std::unordered_map<Key, CacheEntry> cache;
+    //This is a "frequency list" that tracks the accesses/frequency of least used keys
+    //When we access a value with a key, the key is moved from the [nth] bucket to the [n+1] bucket
+    std::unordered_map<int, std::list<Key>> freqMap;
+    //Minfreq keeps track of which buckets of freqList are the least used
+    int minFreq = 0;
+};
